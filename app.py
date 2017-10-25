5779bd504f4e7731a87bae1e6cb56096193a2a95
nikitapandya <npandy18@bu.edu>
2017-10-25 14:35:32 -0400

    cleaned & added try/catch

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import urllib
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 import requests, http.client
 import json
 app = Flask(__name__)
-conn = http.client.HTTPSConnection("api.themoviedb.org")
+
+try:
+    conn = http.client.HTTPSConnection("api.themoviedb.org")
+except Exception as e:
+    print(e.args)
 
 @app.route('/')
 def index():
@@ -18,20 +22,33 @@ def User_Action():
 
 
     payload = "{}"
-    conn.request("GET", "/3/genre/"+ text +"/movies?sort_by=created_at.asc&include_adult=false&language=en-US&api_key=6874ac2dd0d38d7150d4f758d81f6f08", payload)
+    try:
+        conn.request("GET", "/3/genre/"+ text +"/movies?sort_by=created_at.asc&include_adult=false&language=en-US&api_key=6874ac2dd0d38d7150d4f758d81f6f08", payload)
 
 
-    res = conn.getresponse()
-    data = res.read()
+        res = conn.getresponse()
+        data = res.read()
 
-    dataj = json.loads(data.decode())
+        dataj = json.loads(data.decode())   
 
-    #print(data.decode("utf-8"))
+        #print(data.decode("utf-8"))
 
-    titles = ""
-    for i in range(20):
-        titles += dataj['results'][i]['original_title'] + ", "
-    return titles[:-2]
+        titles = ""
+        for i in range(20):
+            titles += dataj['results'][i]['original_title'] + ", "
+        return titles[:-2]
+
+    except Exception as e:
+        print(e.args) 
+
+#for 500 (internal server error) and 404 error
+@app.errorhandler(500)
+def internal_error(error):
+    return "Genre doesn't exist"
+
+@app.errorhandler(404)
+def not_found(error):
+    return "404 error",404
 
 if __name__ == '__main__':
     app.run()
